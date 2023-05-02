2fc1af94c09c946541549cb00a4bb25e8b427aef
jbeltrami <joaopbeltrami@gmail.com>
2023-05-02 16:20:02 -0300

    Include user info in getAll return from TRPCRouter
    Use clerkClient, to check for user data on the on posts router.
    Map the author  to the post by finding user id matches in post.authorId.
    Throw an error, if it doesn't find an author for a post.
    Return post data and author data in the same object.
    Create a PostView component to render returned data from the api.
    Create a PostWizard component with no functionality.
    Wizard will be used, so a user can create new posts.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,67 @@
 import { SignIn, SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
+// import {Image} from 'next/image/'
 import Head from "next/head";
 import Link from "next/link";
 
-import { api } from "~/utils/api";
+import { api, type RouterOutputs } from "~/utils/api";
+
+const CreatePostWizard = () => {
+  const { user } = useUser();
+  console.log(user);
+
+  if (!user) return null;
+
+  return (
+    <div className="flex w-full gap-3">
+      <img
+        src={user.profileImageUrl}
+        alt="Profile Image"
+        className="h-14 w-14 rounded-full"
+      />
+
+      <input
+        placeholder="Type some emojis!"
+        className="grow bg-transparent outline-none"
+      />
+    </div>
+  );
+};
+
+type PostWithUser = RouterOutputs["posts"]["getAll"][number];
+
+const PostVIew = (props: PostWithUser) => {
+  const { post, author } = props;
+  return (
+    <div
+      className="flex items-center gap-x-3 border-b border-slate-400 p-4 align-middle"
+      key={post.id}
+    >
+      <img
+        src={author.profileImageUrl}
+        alt="Profile Image"
+        className="h-14 w-14 rounded-full"
+      />
+      <div className="flex flex-col">
+        <div className="flex gap-3 text-slate-300">
+          <span>{`@${author.username}`}</span>
+          <span>·</span>
+          <span>1 hour ago</span>
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: post.content }} />
+      </div>
+    </div>
+  );
+};
 
 const Home: NextPage = () => {
   const user = useUser();
 
-  const { data } = api.posts.getAll.useQuery();
+  const { data, isLoading } = api.posts.getAll.useQuery();
+
+  if (isLoading) return <div>Loading ...</div>;
+
+  if (!data) return <div>Something went wrong</div>;
 
   return (
     <>
@@ -18,19 +71,21 @@ const Home: NextPage = () => {
         <meta charSet="UTF-8" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div>
-          {!user.isSignedIn && <SignInButton />}
-          {!!user.isSignedIn && <SignOutButton />}
-        </div>
-        {/* <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" /> */}
-        <div>
-          {data?.map((post) => (
-            <div
-              dangerouslySetInnerHTML={{ __html: post.content }}
-              key={post.id}
-            />
-          ))}
+      <main className="flex h-screen justify-center">
+        <div className="h-full w-full  border-x border-slate-400 md:max-w-2xl">
+          <div className="flex border-b border-slate-400 p-4">
+            <div className="flex justify-center">
+              {!user.isSignedIn && <SignInButton />}
+              {/* {!!user.isSignedIn && <SignOutButton />} */}
+            </div>
+            {!!user.isSignedIn && <CreatePostWizard />}
+          </div>
+          {/* <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" /> */}
+          <div className="flex flex-col">
+            {data?.map((fullPost) => (
+              <PostVIew {...fullPost} key={fullPost.post.id} />
+            ))}
+          </div>
         </div>
       </main>
     </>
