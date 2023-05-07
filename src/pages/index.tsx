ae1eb808791e6e19ca2a4fe6b37b843e0f27babd
jBeltrami <joaopbeltrami@gmail.com>
2023-05-07 11:43:50 -0300

    Install dayjs, use next/image tag
    Update images with Next's optimized image component.
    Pass Clerk's domain in next.config.js to accept images from a different server.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
-import { SignIn, SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
+import { SignInButton, useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
 // import {Image} from 'next/image/'
+import dayjs from "dayjs";
+import relativeTime from 'dayjs/plugin/relativeTime';
 import Head from "next/head";
 import Link from "next/link";
 
+import Image from "next/image";
 import { api, type RouterOutputs } from "~/utils/api";
+
+dayjs.extend(relativeTime);
 
 const CreatePostWizard = () => {
   const { user } = useUser();
@@ -14,10 +19,12 @@ const CreatePostWizard = () => {
 
   return (
     <div className="flex w-full gap-3">
-      <img
+      <Image
         src={user.profileImageUrl}
         alt="Profile Image"
         className="h-14 w-14 rounded-full"
+        width={56}
+        height={56}
       />
 
       <input
@@ -37,16 +44,18 @@ const PostVIew = (props: PostWithUser) => {
       className="flex items-center gap-x-3 border-b border-slate-400 p-4 align-middle"
       key={post.id}
     >
-      <img
+      <Image
         src={author.profileImageUrl}
-        alt="Profile Image"
+        alt={`@${author.username}'s profile picture`}
         className="h-14 w-14 rounded-full"
+        width={56}
+        height={56}
       />
       <div className="flex flex-col">
-        <div className="flex gap-3 text-slate-300">
+        <div className="flex gap-1 text-slate-300">
           <span>{`@${author.username}`}</span>
           <span>·</span>
-          <span>1 hour ago</span>
+          <span className="font-thin">{dayjs(post.createdAt).fromNow()}</span>
         </div>
         <div dangerouslySetInnerHTML={{ __html: post.content }} />
       </div>
